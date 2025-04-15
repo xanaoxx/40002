fc96e3336ad913c31b69b61e3065fd43df5b8b7c
Ivashin <danya001406@gmail.com>
2025-04-16 02:43:22 +0300

    back

--- ivashin.dannil/T2/main.cpp
+++ ivashin.dannil/T2/main.cpp
@@ -19,25 +19,33 @@ std::istream& operator>>(std::istream& is, DataStruct& ds) {
     }
 
     std::istringstream iss(line);
+    iss >> std::ws; // Пропускаем начальные пробелы
     char c;
-    if (!(iss >> c) || c != '(') return is.setstate(std::ios::failbit), is;
+    if (!(iss >> c) || c != '(') {
+        is.setstate(std::ios::failbit);
+        return is;
+    }
 
     unsigned long long k1 = 0, k2 = 0;
     std::string k3;
     bool has_k1 = false, has_k2 = false, has_k3 = false;
-    std::string token;
 
     while (iss >> c && c == ':') {
+        std::string token;
         if (!(iss >> token)) break;
         if (token == "key1") {
             if (!(iss >> k1)) break;
             std::string suffix;
-            iss >> suffix;
-            if (suffix != "ull" && suffix != "ULL") break;
+            if (!(iss >> suffix) || (suffix != "ull" && suffix != "ULL")) break;
             has_k1 = true;
         }
         else if (token == "key2") {
-            if (!(iss >> std::hex >> k2)) break;
+            iss >> std::ws;
+            std::string hex_val;
+            if (!(iss >> hex_val)) break;
+            if (hex_val.substr(0, 2) != "0x" && hex_val.substr(0, 2) != "0X") break;
+            std::istringstream hex_stream(hex_val.substr(2));
+            if (!(hex_stream >> std::hex >> k2)) break;
             has_k2 = true;
         }
         else if (token == "key3") {
@@ -49,9 +57,11 @@ std::istream& operator>>(std::istream& is, DataStruct& ds) {
         else {
             break;
         }
+        iss >> std::ws;
         if (!(iss >> c) || c != ':') break;
     }
 
+    iss >> std::ws;
     if (has_k1 && has_k2 && has_k3 && iss >> c && c == ')') {
         ds.key1 = k1;
         ds.key2 = k2;
@@ -76,14 +86,21 @@ int main() {
         std::istream_iterator<DataStruct>(),
         std::back_inserter(data));
 
-    std::sort(data.begin(), data.end(), [](const DataStruct& a, const DataStruct& b) {
-        if (a.key1 != b.key1) return a.key1 < b.key1;
-        if (a.key2 != b.key2) return a.key2 < b.key2;
-        return a.key3.length() < b.key3.length();
-        });
+    if (data.empty()) {
+        std::cout << "Looks like there is no supported record. "
+            << "Cannot determine input. Test skipped\n";
+    }
+    else {
+        std::cout << "Atleast one supported record type\n";
+        std::sort(data.begin(), data.end(), [](const DataStruct& a, const DataStruct& b) {
+            if (a.key1 != b.key1) return a.key1 < b.key1;
+            if (a.key2 != b.key2) return a.key2 < b.key2;
+            return a.key3.length() < b.key3.length();
+            });
 
-    std::copy(data.begin(), data.end(),
-        std::ostream_iterator<DataStruct>(std::cout, "\n"));
+        std::copy(data.begin(), data.end(),
+            std::ostream_iterator<DataStruct>(std::cout, "\n"));
+    }
 
     std::cout << std::endl;
     return 0;
