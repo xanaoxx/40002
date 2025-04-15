5aebe35df2aed806d676fc30e81240f38d8bd343
rekottt <viceone.von@gmail.com>
2025-04-15 21:04:53 +0300

    Update DataStruct.h

--- karyakin.platon/T2/DataStruct.h
+++ karyakin.platon/T2/DataStruct.h
@@ -1,5 +1,3 @@
-﻿#pragma once
-
 #ifndef DATASTRUCT_H
 #define DATASTRUCT_H
 
@@ -75,4 +73,4 @@ namespace nspace
     std::ostream& operator<<(std::ostream& out, const DataStruct& dest);
 }
 
-#endif !DATASTRUCT_H
+#endif //!DATASTRUCT_H
