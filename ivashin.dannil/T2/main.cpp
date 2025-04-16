6723890c3a49e5cd476890f182893229325dc884
Ivashin <danya001406@gmail.com>
2025-04-16 03:19:28 +0300

    fix

--- ivashin.dannil/T2/main.cpp
+++ ivashin.dannil/T2/main.cpp
@@ -1,135 +1,170 @@
 #include <algorithm>
+#include <iomanip>
 #include <iostream>
 #include <iterator>
 #include <sstream>
 #include <string>
 #include <vector>
-#include <iomanip>
 
-namespace nspace {
+struct DataStruct
+{
+    unsigned long long key1_;
+    unsigned long long key2_;
+    std::string key3_;
+};
 
-    struct DataStruct {
-        unsigned long long key1;
-        unsigned long long key2;
-        std::string key3;
-    };
-
-    class iofmtguard {
-    public:
-        iofmtguard(std::basic_ios<char>& s) : s_(s), fmt_(s.flags()) {}
-        ~iofmtguard() { s_.flags(fmt_); }
-    private:
-        std::basic_ios<char>& s_;
-        std::ios_base::fmtflags fmt_;
-    };
-
-    std::istream& operator>>(std::istream& is, DataStruct& ds) {
-        std::istream::sentry sentry(is);
-        if (!sentry) return is;
-
-        DataStruct temp;
-        std::vector<bool> keys(3, false); // key1, key2, key3
-        char c;
-        std::string token;
-
-        if (!(is >> c) || c != '(') {
-            is.setstate(std::ios::failbit);
-            return is;
-        }
-
-        while (is >> c && c == ':') {
-            if (!(is >> token)) {
-                is.setstate(std::ios::failbit);
-                break;
-            }
-
-            if (token == "key1" && !keys[0]) {
-                unsigned long long val;
-                std::string suffix;
-                if (!(is >> val >> suffix) || (suffix != "ull" && suffix != "ULL")) {
-                    is.setstate(std::ios::failbit);
-                    break;
-                }
-                temp.key1 = val;
-                keys[0] = true;
-            }
-            else if (token == "key2" && !keys[1]) {
-                std::string hex_str;
-                if (!(is >> hex_str) || hex_str.size() < 3 || hex_str.substr(0, 2) != "0x") {
-                    is.setstate(std::ios::failbit);
-                    break;
-                }
-                std::istringstream iss(hex_str.substr(2));
-                unsigned long long val;
-                if (!(iss >> std::hex >> val)) {
-                    is.setstate(std::ios::failbit);
-                    break;
-                }
-                temp.key2 = val;
-                keys[1] = true;
-            }
-            else if (token == "key3" && !keys[2]) {
-                is >> std::ws;
-                if (is.get() != '"') {
-                    is.setstate(std::ios::failbit);
-                    break;
-                }
-                std::getline(is, temp.key3, '"');
-                keys[2] = true;
-            }
-            else {
-                is.setstate(std::ios::failbit);
-                break;
-            }
-        }
-
-        if (is && is >> c && c == ')' && keys[0] && keys[1] && keys[2]) {
-            ds = temp;
-        }
-        else {
-            is.setstate(std::ios::failbit);
-        }
-
-        return is;
+class IofmtGuard
+{
+public:
+    IofmtGuard(std::basic_ios<char>& stream)
+        : stream_(stream)
+        , flags_(stream.flags())
+    {
     }
 
-    std::ostream& operator<<(std::ostream& os, const DataStruct& ds) {
-        std::ostream::sentry sentry(os);
-        if (!sentry) return os;
-
-        iofmtguard fmtguard(os);
-        os << "(:key1 " << ds.key1 << "ull:key2 0x" << std::hex << std::uppercase
-            << ds.key2 << std::dec << ":key3 \"" << ds.key3 << "\":)";
-        return os;
+    ~IofmtGuard()
+    {
+        stream_.flags(flags_);
     }
 
-} 
+private:
+    std::basic_ios<char>& stream_;
+    std::ios_base::fmtflags flags_;
+};
 
-using nspace::DataStruct;
+std::istream& operator>>(std::istream& input, DataStruct& data)
+{
+    std::istream::sentry sentry(input);
+    if (!sentry)
+    {
+        return input;
+    }
 
-bool cmp(const DataStruct& a, const DataStruct& b) {
-    if (a.key1 != b.key1) return a.key1 < b.key1;
-    if (a.key2 != b.key2) return a.key2 < b.key2;
-    return a.key3.length() < b.key3.length();
+    DataStruct temp;
+    std::vector<bool> keysPresent(3, false);
+    char c;
+    std::string token;
+
+    if (!(input >> c) || c != '(')
+    {
+        input.setstate(std::ios::failbit);
+        return input;
+    }
+
+    while (input >> c && c == ':')
+    {
+        if (!(input >> token))
+        {
+            input.setstate(std::ios::failbit);
+            break;
+        }
+
+        if (token == "key1" && !keysPresent[0])
+        {
+            unsigned long long value;
+            std::string suffix;
+            if (!(input >> value >> suffix) || (suffix != "ull" && suffix != "ULL"))
+            {
+                input.setstate(std::ios::failbit);
+                break;
+            }
+            temp.key1_ = value;
+            keysPresent[0] = true;
+        }
+        else if (token == "key2" && !keysPresent[1])
+        {
+            std::string hexString;
+            if (!(input >> hexString) || hexString.size() < 3 || hexString.substr(0, 2) != "0x")
+            {
+                input.setstate(std::ios::failbit);
+                break;
+            }
+            std::istringstream iss(hexString.substr(2));
+            unsigned long long value;
+            if (!(iss >> std::hex >> value))
+            {
+                input.setstate(std::ios::failbit);
+                break;
+            }
+            temp.key2_ = value;
+            keysPresent[1] = true;
+        }
+        else if (token == "key3" && !keysPresent[2])
+        {
+            input >> std::ws;
+            if (input.get() != '"')
+            {
+                input.setstate(std::ios::failbit);
+                break;
+            }
+            std::getline(input, temp.key3_, '"');
+            keysPresent[2] = true;
+        }
+        else
+        {
+            input.setstate(std::ios::failbit);
+            break;
+        }
+    }
+
+    if (input && input >> c && c == ')' && keysPresent[0] && keysPresent[1] && keysPresent[2])
+    {
+        data = temp;
+    }
+    else
+    {
+        input.setstate(std::ios::failbit);
+    }
+
+    return input;
 }
 
-int main() {
+std::ostream& operator<<(std::ostream& output, const DataStruct& data)
+{
+    std::ostream::sentry sentry(output);
+    if (!sentry)
+    {
+        return output;
+    }
+
+    IofmtGuard guard(output);
+    output << "(:key1 " << data.key1_ << "ull:key2 0x" << std::hex << std::uppercase
+        << data.key2_ << std::dec << ":key3 \"" << data.key3_ << "\":)";
+    return output;
+}
+
+bool compareData(const DataStruct& first, const DataStruct& second)
+{
+    if (first.key1_ != second.key1_)
+    {
+        return first.key1_ < second.key1_;
+    }
+    if (first.key2_ != second.key2_)
+    {
+        return first.key2_ < second.key2_;
+    }
+    return first.key3_.length() < second.key3_.length();
+}
+
+int main()
+{
     std::vector<DataStruct> data;
 
-    // We read the data ignoring incorrect lines
-    while (!std::cin.eof()) {
+    while (!std::cin.eof())
+    {
         std::copy(
             std::istream_iterator<DataStruct>(std::cin),
             std::istream_iterator<DataStruct>(),
             std::back_inserter(data)
         );
-        if (!std::cin) {
+        if (!std::cin)
+        {
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         }
     }
 
-    std::sort(data.begin(), data.end(), cmp);
+    std::sort(data.begin(), data.end(), compareData);
 
     std::copy(
         data.begin(),
