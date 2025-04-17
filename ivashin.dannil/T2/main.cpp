4b1d9737761908d9444ed83273014493241f1939
Ivashin <danya001406@gmail.com>
2025-04-18 01:07:58 +0300

    fix pars

--- ivashin.dannil/T2/main.cpp
+++ ivashin.dannil/T2/main.cpp
@@ -12,107 +12,91 @@ struct DataStruct {
     std::string key3_;
 };
 
+// Класс для сохранения и восстановления формата потока
 class IofmtGuard {
 public:
     IofmtGuard(std::basic_ios<char>& stream) : stream_(stream), flags_(stream.flags()) {}
     ~IofmtGuard() { stream_.flags(flags_); }
-
 private:
     std::basic_ios<char>& stream_;
     std::ios_base::fmtflags flags_;
 };
 
-std::istream& operator>>(std::istream& input, DataStruct& data) {
-    std::istream::sentry sentry(input);
-    if (!sentry) {
-        return input;
+bool parseDataStruct(const std::string& line, DataStruct& data) {
+    std::istringstream iss(line);
+    char c;
+    if (!(iss >> c) || c != '(') {
+        return false;
     }
 
     DataStruct temp;
-    char c;
-    std::string token;
-
-    if (!(input >> c) || c != '(') {
-        input.setstate(std::ios::failbit);
-        return input;
-    }
-
-    bool valid = true;
     bool keysPresent[3] = { false, false, false };
 
-    while (valid && input >> c && c == ':') {
-        if (!(input >> token)) {
-            valid = false;
-            break;
+    while (iss.peek() == ':') {
+        iss.ignore(1, ':'); // Пропускаем ':'
+        if (iss.peek() == ')') {
+            break; 
         }
-
+        std::string token;
+        if (!(iss >> token)) {
+            return false;
+        }
         if (token == "key1" && !keysPresent[0]) {
             std::string valueStr;
-            if (!(input >> valueStr)) {
-                valid = false;
-                break;
+            if (!(iss >> valueStr)) {
+                return false;
             }
-            // Проверка формата ULL LIT (например, "123ull" или "123ULL")
+            // Проверка формата ULL LIT
             if (valueStr.size() < 4 ||
                 (valueStr.substr(valueStr.size() - 3) != "ull" && valueStr.substr(valueStr.size() - 3) != "ULL")) {
-                valid = false;
-                break;
+                return false;
             }
-            std::istringstream iss(valueStr.substr(0, valueStr.size() - 3));
+            std::istringstream numIss(valueStr.substr(0, valueStr.size() - 3));
             unsigned long long value;
-            if (!(iss >> value) || iss.rdbuf()->in_avail() > 0) { // Проверка на лишние символы
-                valid = false;
-                break;
+            if (!(numIss >> value) || numIss.rdbuf()->in_avail() > 0) {
+                return false;
             }
             temp.key1_ = value;
             keysPresent[0] = true;
         }
         else if (token == "key2" && !keysPresent[1]) {
             std::string hexString;
-            if (!(input >> hexString)) {
-                valid = false;
-                break;
+            if (!(iss >> hexString)) {
+                return false;
             }
-            // Проверка формата ULL HEX (например, "0xFFA")
+            // Проверка формата ULL HEX
             if (hexString.size() < 3 || hexString.substr(0, 2) != "0x") {
-                valid = false;
-                break;
+                return false;
             }
-            std::istringstream iss(hexString.substr(2));
+            std::istringstream numIss(hexString.substr(2));
             unsigned long long value;
-            if (!(iss >> std::hex >> value) || iss.rdbuf()->in_avail() > 0) { // Проверка на лишние символы
-                valid = false;
-                break;
+            if (!(numIss >> std::hex >> value) || numIss.rdbuf()->in_avail() > 0) {
+                return false;
             }
             temp.key2_ = value;
             keysPresent[1] = true;
         }
         else if (token == "key3" && !keysPresent[2]) {
-            input >> std::ws; // Пропуск пробелов
-            if (input.get() != '"') {
-                valid = false;
-                break;
+            iss >> std::ws;
+            if (iss.get() != '"') {
+                return false;
             }
-            std::getline(input, temp.key3_, '"');
-            if (input.fail()) {
-                valid = false;
-                break;
+            std::getline(iss, temp.key3_, '"');
+            if (iss.fail()) {
+                return false;
             }
             keysPresent[2] = true;
         }
         else {
-            valid = false;
-            break;
+            return false;
         }
     }
-    if (valid && input >> c && c == ')' && keysPresent[0] && keysPresent[1] && keysPresent[2]) {
-        data = temp;
-    }
-    else {
-        input.setstate(std::ios::failbit);
-    }
 
-    return input;
+    if (iss >> c && c == ')' && keysPresent[0] && keysPresent[1] && keysPresent[2]) {
+        data = temp;
+        return true;
+    }
+    return false;
 }
 
 std::ostream& operator<<(std::ostream& output, const DataStruct& data) {
@@ -120,7 +104,6 @@ std::ostream& operator<<(std::ostream& output, const DataStruct& data) {
     if (!sentry) {
         return output;
     }
-
     IofmtGuard guard(output);
     output << "(:key1 " << data.key1_ << "ull:key2 0x" << std::hex << std::uppercase
         << data.key2_ << std::dec << ":key3 \"" << data.key3_ << "\":)";
@@ -140,30 +123,13 @@ bool compareData(const DataStruct& first, const DataStruct& second) {
 int main() {
     std::vector<DataStruct> data;
     bool hasValidRecord = false;
+    std::string line;
 
-    while (!std::cin.eof()) {
+    while (std::getline(std::cin, line)) {
         DataStruct temp;
-        std::cin >> temp;
-        if (std::cin.good()) {
+        if (parseDataStruct(line, temp)) {
             data.push_back(temp);
             hasValidRecord = true;
-        }
-        else if (!std::cin.eof()) {
-            std::cin.clear();
-            char c;
-            while (std::cin.get(c) && c != ')' && c != '\n') {
-                if (c == '(') {
-                    int parenCount = 1;
-                    while (std::cin.get(c) && parenCount > 0) {
-                        if (c == '(') parenCount++;
-                        if (c == ')') parenCount--;
-                    }
-                    break;
-                }
-            }
-            if (c == ')') {
-                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            }
         }
     }
 
