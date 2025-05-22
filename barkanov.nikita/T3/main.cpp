e71b9fba013b792b6d9e527f36c05c05a5a8d0c1
ControlShiftEscape <maytarosheejsh@outlook.com>
2025-05-23 00:02:00 +0300

    Restoring all previous data

--- barkanov.nikita/T3/main.cpp
+++ barkanov.nikita/T3/main.cpp
@@ -1,5 +1,39 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream> //string stream
 
-int main() {
+#include "shapes.h"
+#include "CommandHandler.h"
+
+/*
+    variant 6
+    ECHO MAXSEQ
+*/
+
+int main(int argc, char* argv[]) {
+
+    if (argc != 2) {
+        std::cerr << "ERROR: Exactly one argument must be passed.";
+        return 1;
+    }
+    std::string filename;
+    filename = argv[1];
+
+    std::string line;
+    GeometryHandler cmdHandler;
+
+    cmdHandler.fillMainVector(filename);
+    cmdHandler.printMainVector(std::cout);
+
+    while (std::getline(std::cin, line)) {
+        if (line.empty()) continue;
+        std::istringstream iss(line);
+        std::string cmd;
+        iss >> cmd;
+        cmdHandler.execute(cmd, iss, std::cout);
+    }
+
     return 0;
 }
