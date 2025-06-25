bc6ced2a6f0dc453aeb6bcc2544c08c58347363e
eg0sha-0 <geneziz777@bk.ru>
2025-06-25 22:11:15 +0300

    Init new T2

---
+++ kovalchuk.egor/T2/DataStruct.cpp
@@ -0,0 +1,122 @@
+﻿#include "DataStruct.h"
+
+
+namespace dStruct
+{
+    std::istream& operator>>(std::istream& in, DelimiterIO&& dest)
+    {
+        std::istream::sentry sentry(in);
+        if (!sentry)
+            return in;
+        char c = '0';
+        in >> c;
+        if (in && (c != dest.exp))
+            in.setstate(std::ios::failbit);
+        return in;
+    }
+
+    std::istream& operator>>(std::istream& in, LongLongIOLL&& dest)
+    {
+        std::istream::sentry sentry(in);
+        if (!sentry) return in;
+        return in >> dest.ref >> DelimiterIO{ 'l' } >> DelimiterIO{ 'l' };
+    }
+
+    std::istream& operator>>(std::istream& in, LongLongIO&& dest)
+    {
+        std::istream::sentry sentry(in);
+        if (!sentry) return in;
+        return in >> dest.ref;
+    }
+
+    std::istream& operator>>(std::istream& in, UnsignedLongLongIO&& dest)
+    {
+        std::istream::sentry sentry(in);
+        if (!sentry) return in;
+        return in >> dest.ref;
+    }
+
+    std::istream& operator>>(std::istream& in, PairIO&& dest)
+    {
+        std::istream::sentry sentry(in);
+        if (!sentry) return in;
+        in >> DelimiterIO{ '(' } >> DelimiterIO{ ':' };
+        in >> DelimiterIO{ 'N' } >> LongLongIO{ dest.ref.first } >> DelimiterIO{ ':' };
+        in >> DelimiterIO{ 'D' } >> UnsignedLongLongIO{ dest.ref.second };
+        in >> DelimiterIO{ ':' } >> DelimiterIO{ ')' };
+        return in;
+    }
+
+    std::istream& operator>>(std::istream& in, StringIO&& dest)
+    {
+        std::istream::sentry sentry(in);
+        if (!sentry) return in;
+        return std::getline(in >> DelimiterIO{ '"' }, dest.ref, '"');
+    }
+
+    std::istream& operator>>(std::istream& in, DataStruct& dest)
+    {
+        std::istream::sentry sentry(in);
+        if (!sentry) return in;
+        DataStruct input;
+        in >> DelimiterIO{ '(' };
+        bool hasKey1 = false, hasKey2 = false, hasKey3 = false;
+        std::string key;
+
+
+        while (in >> DelimiterIO{ ':' } && in >> key)
+        {
+            if (key == ")") {
+                in.putback(')');
+                break;
+            }
+            if (key == "key1") {
+                in >> LongLongIOLL{ input.key1 };
+                hasKey1 = true;
+            }
+            else if (key == "key2") {
+                in >> PairIO{ input.key2 };
+                hasKey2 = true;
+            }
+            else if (key == "key3") {
+                in >> StringIO{ input.key3 };
+                hasKey3 = true;
+            }
+        }
+        in >> DelimiterIO{ ')' };
+        if (in && hasKey1 && hasKey2 && hasKey3)
+            dest = input;
+        else
+            in.setstate(std::ios::failbit);
+        return in;
+
+    }
+
+    std::ostream& operator<<(std::ostream& out, const DataStruct& src)
+    {
+        std::ostream::sentry sentry(out);
+        if (!sentry) return out;
+        iofmtguard fmtguard(out);
+        out << "(:key1 " << src.key1 << "ll";
+        out << ":key2 (:N " << src.key2.first << ":D " << src.key2.second << ":)";
+        out << ":key3 \"" << src.key3 << "\":)";
+        return out;
+    }
+
+    iofmtguard::iofmtguard(std::basic_ios<char>& s) :
+        s_(s),
+        width_(s.width()),
+        fill_(s.fill()),
+        precision_(s.precision()),
+        fmt_(s.flags())
+    {
+    }
+
+    iofmtguard::~iofmtguard()
+    {
+        s_.width(width_);
+        s_.fill(fill_);
+        s_.precision(precision_);
+        s_.flags(fmt_);
+    }
+}
