a7ef297613acf486cef036893744f9624bcf5c5e
xanaoxx <znellaz25@gmail.com>
2025-09-18 04:49:42 +0300

    add operators

--- varfolomeeva.kseniya/T2/DataStruct.cpp
+++ varfolomeeva.kseniya/T2/DataStruct.cpp
@@ -12,4 +12,29 @@ namespace dStruct
 
         return in >> key.value >> DelimiterIO{ 'l' } >> DelimiterIO{ 'l' };
     }
+    std::istream& operator>>(std::istream& in, DelimiterIO&& dest) {
+        std::istream::sentry sentry(in);
+        if (!sentry)
+           return in;
+        char c = '0';
+        in >> c;
+        if (in && (c != dest.exp))
+            in.setstate(std::ios::failbit);
+        return in;
+    }
+     std::ostream& operator<<(std::ostream& out, const DataStruct& value) {
+     std::ostream::sentry sentry(out);
+     if (!sentry) {
+         return out;
+     }
+     iofmtguard fmtguard(out);
+
+     out << std::setprecision(1) << std::fixed << "(";
+     out << ":key1 " << value.key1 << "d";
+     out << ":key2 " << value.key2 << "ll";
+     out << ":key3 \"" << value.key3 << "\":)";
+
+     return out;
+ }
+
 }
