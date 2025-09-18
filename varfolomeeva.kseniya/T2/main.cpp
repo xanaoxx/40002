b8615ec69f51c6ea9d59e21475cdcd0bb90019d1
xanaoxx <znellaz25@gmail.com>
2025-09-18 05:16:35 +0300

    modified file

--- varfolomeeva.kseniya/T2/main.cpp
+++ varfolomeeva.kseniya/T2/main.cpp
@@ -5,6 +5,18 @@ int main(){
     using dStruct::DataStruct;
     std::vector<DataStruct> data;
     while (!std::cin.eof()){
-        if(!std::cin){}
-}
+            if (!std::cin)
+    {
+        std::cin.clear();
+    }
+    std::copy(
+        std::istream_iterator<DataStruct>(std::cin),
+        std::istream_iterator<DataStruct>(),
+        std::back_inserter(data)
+    );
+    if (!std::cin)
+    {
+        std::cin.ignore(100, '\n');
+    }
+
 }
