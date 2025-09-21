72eaa11fdee82dbb8202100b4baffd2fb603db03
xanaoxx <znellaz25@gmail.com>
2025-09-21 23:04:04 +0300

    Move T3 files to correct directory

--- commands.hpp
+++
@@ -1,5 +0,0 @@
-#ifndef COMMANDS_HPP
-#define COMMANDS_HPP
-#include "figures.hpp"
-
-#endif
--- figures.cpp
+++
@@ -1,4 +0,0 @@
-#include "figures.hpp"
-#include <iterator>
-#include <algorithm>
-#include "delimiter.hpp"
