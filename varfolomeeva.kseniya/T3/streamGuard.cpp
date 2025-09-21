b472431f781401520f2ab9e9e86615d228fdb136
xanaoxx <znellaz25@gmail.com>
2025-09-22 01:24:51 +0300

    modify file

--- varfolomeeva.kseniya/T3/streamGuard.cpp
+++ varfolomeeva.kseniya/T3/streamGuard.cpp
@@ -1,1 +1,12 @@
 #include "streamGuard.hpp"
+
+varfolomeeva::StreamGuard::StreamGuard(std::basic_ios<char>& s) :
+    s_(s),
+    precision_(s.precision()),
+    flags_(s.flags())
+{}
+
+varfolomeeva::StreamGuard::~StreamGuard() {
+    s_.precision(precision_);
+    s_.flags(flags_);
+}
