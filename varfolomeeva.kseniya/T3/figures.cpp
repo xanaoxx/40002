7ea8085379102e0ff36bd735f6a9ec23a9ef28ad
xanaoxx <znellaz25@gmail.com>
2025-09-22 01:17:48 +0300

    modify file

--- varfolomeeva.kseniya/T3/figures.cpp
+++ varfolomeeva.kseniya/T3/figures.cpp
@@ -18,3 +18,45 @@ std::istream& varfolomeeva::operator>>(std::istream& in, Point& point)
     }
     return in;
 }
+std::istream& varfolomeeva::operator>>(std::istream& in, Polygon& poly)
+{
+    std::istream::sentry guard(in);
+    if (!guard)
+    {
+        return in;
+    }
+    size_t vertexes = 0;
+    in >> vertexes;
+    if (vertexes < 3)
+    {
+        in.setstate(std::ios::failbit);
+        return in;
+    }
+    std::vector< Point > temp;
+    using inIterator = std::istream_iterator< Point >;
+    std::copy_n(inIterator{ in }, vertexes, std::back_inserter(temp));
+    if (in && temp.size() == vertexes)
+    {
+        poly.points = temp;
+    }
+    else
+    {
+        in.setstate(std::ios::failbit);
+    }
+    return in;
+}
+
+bool varfolomeeva::operator!=(const Polygon& p1, const Polygon& p2)
+{
+    return p1.points != p2.points;
+}
+
+bool varfolomeeva::operator==(const Point& p1, const Point& p2)
+{
+    return (p1.x == p2.x) && (p1.y == p2.y);
+}
+
+bool varfolomeeva::operator==(const Polygon& p1, const Polygon& p2)
+{
+    return p1.points == p2.points;
+}
