ea314edf1ef439e55f6a3b26dabca5d2c1c967f4
xanaoxx <znellaz25@gmail.com>
2025-09-21 22:59:33 +0300

    Edit file

--- varfolomeeva.kseniya/T3/figures.cpp
+++ varfolomeeva.kseniya/T3/figures.cpp
@@ -2,3 +2,19 @@
 #include <iterator>
 #include <algorithm>
 #include "delimiter.hpp"
+std::istream& varfolomeeva::operator>>(std::istream& in, Point& point)
+{
+    std::istream::sentry guard(in);
+    if (!guard)
+    {
+        return in;
+    }
+    using del = Delimiter;
+    Point temp{ 0, 0 };
+    in >> del{ '(' } >> temp.x >> del{ ';' } >> temp.y >> del{ ')' };
+    if (in)
+    {
+        point = temp;
+    }
+    return in;
+}
