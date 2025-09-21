72468d5fa371085f40a6a77e7da3fc0288355689
xanaoxx <znellaz25@gmail.com>
2025-09-22 01:28:15 +0300

    modify file

--- varfolomeeva.kseniya/T3/main.cpp
+++ varfolomeeva.kseniya/T3/main.cpp
@@ -2,8 +2,34 @@
 int main()
 {
     using namespace varfolomeeva;
-    if (argc != 2)
+if (argc != 2)
 {
     std::cerr << "<INVALID ARGUMENTS>\n";
     return 1;
+}
+std::ifstream file(argv[1]);
+if (!file.is_open())
+{
+    std::cerr << "<INVALID FILE>\n";
+    return 1;
+}
+std::vector< Polygon > polygons;
+using inIterator = std::istream_iterator< Polygon >;
+while (!file.eof())
+{
+    std::copy(inIterator{ file }, inIterator{}, std::back_inserter(polygons));
+    if (file.fail())
+    {
+        file.clear();
+        file.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+}
+file.close();
+std::map<std::string, std::function<void(std::istream&, std::ostream&)>> cmds;
+using namespace std::placeholders;
+cmds["AREA"] = std::bind(doArea, std::ref(polygons), _1, _2);
+cmds["MAX"] = std::bind(doMax, std::ref(polygons), _1, _2);
+cmds["MIN"] = std::bind(doMin, std::ref(polygons), _1, _2);
+cmds["COUNT"] = std::bind(doCount, std::ref(polygons), _1, _2);
+cmds["RMECHO"] = std::bind(doRmecho, std::ref(polygons), _1, _2);
 }
